1bb846bb28b4570ec404ba9df42b6c325f576605
Alexander Muriithi <almuriit@microsoft.com>
2019-11-27 18:13:57 -0800

    Adding jest

--- Card.ts
+++ Card.ts
@@ -1,8 +1,6 @@
 export default class Card {
-
     public static readonly CardTypes: Array<string> = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-    
     public static readonly CardSuites: Array<string> = ['Heart', 'Diamond', 'Spade', 'Club']
 
     private letter: string

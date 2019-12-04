b1d8309667201d4ba3da0f27935ea6dc28290d39
Alexander Muriithi <almuriit@microsoft.com>
2019-12-04 14:48:28 -0800

    Completed tests for Player class

--- tests/Player.test.ts
+++ tests/Player.test.ts
@@ -2,13 +2,22 @@ import Player from '../src/Player';
 import Card from '../src/Card';
 import Deck from '../src/Deck';
 
-let player: Player = new Player("Alex");
+let player: Player;
+let deck: Deck;
 
-let deck: Deck = new Deck();
+beforeEach(() => {
+    player = new Player("Test");
+    deck = new Deck();
+});
+
+afterEach(() => {
+    player = null;
+    deck = null;
+})
 
 test('New Player is instance of class Player', () => {
     expect( player ).toBeInstanceOf(Player);
-    expect( player.getName() ).toBe("Alex");
+    expect( player.getName() ).toBe("Test");
     expect( player.getHand().length ).toBe(0);
     expect( player.getScore() ).toBe(0);
 });
@@ -20,17 +29,15 @@ test('Should increase players hand by one', () => {
 });
 
 test('Should automatically set Ace to 1', () => {
-    let player: Player = new Player("Test")
     player.hitMe(new Card('A', 'Spade'))
     expect( player.getScore() ).toBe(11)
-    player.hitMe(new Card('J', 'Heart'))
-    expect( player.getScore() ).toBe(21)
-    player.hitMe(new Card('2', 'Heart'))
-    expect( player.getScore() ).toBe(13)
-})
+    player.hitMe(new Card('8', 'Heart'))
+    expect( player.getScore() ).toBe(19)
+    player.hitMe(new Card('10', 'Heart'))
+    expect( player.getScore() ).toBe(19)
+});
 
 test('Should set one Ace to 11 and all others to 1', () => {
-    let player: Player = new Player("Test")
     player.hitMe(new Card('A', 'Spade'))
     expect( player.getScore() ).toBe(11)
     player.hitMe(new Card('A', 'Spade'))
@@ -42,6 +49,12 @@ test('Should set one Ace to 11 and all others to 1', () => {
 })
 
 test('Should return player object to original state', () => {
-    let player: Player = new Player("Test")
-    player.hitMe(new Card('A', 'Spade'))
-})
+    player.hitMe( deck.deal() )
+    player.hitMe( deck.deal() )
+    player.hitMe( deck.deal() )
+    expect( player.getHand().length ).toBe(3)
+    player.reset()
+    expect( player.getHand().length ).toBe(0)
+    expect( player.getScore() ).toBe(0)
+    expect( player.getName() ).toBe("Test");
+});
